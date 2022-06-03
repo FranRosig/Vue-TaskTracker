d08add7a2e01df989ff6876043e76340d9a53b88
Franco Rosignuolo <franco_ros@live.com.ar>
2022-06-03 01:37:32 -0300

    Hi

---
+++ src/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go Back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
+
+<style scoped>
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    animation: fadeIn 1s;
+  }
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+</style>
