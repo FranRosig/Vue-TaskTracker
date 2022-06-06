a63622bb98bf50ab948d4cc4cfe3f914803835f6
Franco Rosignuolo <franco_ros@live.com.ar>
2022-06-06 20:13:54 -0300

    Modif for deployment

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vue-tasktrackerr-backend.herokuapp.com',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' },
